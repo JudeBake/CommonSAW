ae8cbb18f82845da16a0d71f21378dd38ec2c215
JudeBake <julien.bacon.1@gmail.com>
2014-01-24 13:23:34 -0500

    setting linking to libCommonCPP headers. setting up doxygen (work in progress).

--- StockValue/StockValue.cpp
+++ StockValue/StockValue.cpp
@@ -11,8 +11,6 @@
  * \version Commit Id: &REVISION&
  */
 
-#include <ctime>
-
 #include "StockValue.h"
 
 namespace StockAction
@@ -21,32 +19,48 @@ namespace StockAction
 StockValue::StockValue()
 {
 	value = 0;
-	timePoint.setTime(time(NULL));
+	timePoint = DateTime::getCurrentDateTime();
 }
 
 StockValue::StockValue(const double iValue)
 {
 	value = iValue;
-	timePoint.setTime(time(NULL));
+	timePoint = DateTime::getCurrentDateTime();
 }
 
 StockValue::StockValue(const double iValue, const DateTime& iTime)
 {
 	value = iValue;
-	timePoint = iTime;
+	timePoint = new DateTime();
+
+	if (timePoint)
+	{
+		*timePoint = iTime;
+	}
 }
 
 StockValue::StockValue(const StockValue& iStockValue)
 {
 	value = iStockValue.value;
-	timePoint = iStockValue.timePoint;
+	timePoint = new DateTime();
+
+	if (timePoint)
+	{
+		*timePoint = iStockValue.timePoint;
+	}
+
 }
 
 StockValue& StockValue::operator=(StockValue iStockValue)
 {
-	//TODO: verify syntax = operator override (how to create and return a ref object.)
-	StockValue& newStockValue = new StockValue(iStockValue);
-	return newStockValue;
+	value = iStockValue.value;
+
+	if (iStockValue.isValid())
+	{
+		*timePoint = iStockValue.timePoint;
+	}
+
+	return *this;
 }
 
 void StockValue::setValue(const double iValue)
@@ -67,6 +81,23 @@ void StockValue::setTime(const DateTime& iTime)
 DateTime& StockValue::getTime(void)
 {
 	return timePoint;
+}
+
+bool StockValue::isValid(void)
+{
+	bool validity = true;
+
+	if (timePoint)
+	{
+		validity = false;
+	}
+
+	return timePoint;
+}
+
+virtual StockValue::~StockValue(void)
+{
+	delete timePoint;
 }
 
 } /* namespace StockAction */
--- StockValue/StockValue.h
+++ StockValue/StockValue.h
@@ -1,6 +1,6 @@
 /**
  * \file StockValue.h
- * \class StockValue
+ * \class StockAction::StockValue
  * \brief Representation of the value of a stock action at a given time.
  *
  * This class is the representation of a stock action's value at a given time.
@@ -30,27 +30,29 @@ namespace StockAction
 class StockValue
 {
 private:
-	/* the value of the stock action (CAD) */
+	/**
+	 * the value of the stock action (CAD)
+	 */
 	double value;
 	/* the given time of that value */
-	DateTime timePoint;
+	DateTime* timePoint = NULL;
 
 public:
-	/*
+	/**
 	 * \brief Basic constructor of StockValue.
 	 *
 	 * Initialize value to 0 and time at current time.
 	 */
 	StockValue(void);
 
-	/*
+	/**
 	 * \brief StockValue constructor with a given initial value at current time.
 	 *
 	 * \param iValue The given value at which the StockValue must be initialized.
 	 */
 	StockValue(const double iValue);
 
-	/*
+	/**
 	 * \brief StockValue constructor with a given value and time.
 	 *
 	 * \param iValue The given value at which the StockValue must be initialized.
@@ -58,15 +60,15 @@ public:
 	 */
 	StockValue(const double iValue, const DateTime& iTime);
 
-	/*
-	 * \brief StockValue constructor from a StockValue reference.
+	/**
+	 * \brief StockValue copy constructor.
 	 *
 	 * \param &iStockValue The reference StockValue from which initialized.
 	 */
 	StockValue(const StockValue& iStockValue);
 
-	/*
-	 * \brief StockValue copy constructor (= operator override).
+	/**
+	 * \brief StockValue assignment operator.
 	 *
 	 * \param iStockValue The StockValue to copy.
 	 *
@@ -74,33 +76,45 @@ public:
 	 */
 	StockValue& operator=(StockValue iStockValue);
 
-	/*
+	/**
 	 * \brief StockValue value setter.
 	 *
 	 * \param iValue The new value of the StockValue (CAD).
 	 */
 	void setValue(const double iValue);
 
-	/*
+	/**
 	 * \brief StockValue value getter.
 	 *
 	 * \return The current value of the StockValue (CAD).
 	 */
 	double getValue(void);
 
-	/*
+	/**
 	 * \brief StockValue time setter.
 	 *
 	 * \param iTime The new time of the StockValue.
 	 */
 	void setTime(const DateTime& iTime);
 
-	/*
+	/**
 	 * \brief StockValue time getter.
 	 *
 	 * \return The current time of the StockValue.
 	 */
 	DateTime& getTime(void);
+
+	/**
+	 * \brief StockValue validity checker.
+	 *
+	 * \return True if the StockValue is valid, False otherwise.
+	 */
+	bool isValid(void);
+
+	/**
+	 * \brief StockValue destructor.
+	 */
+	virtual ~StockValue(void);
 };
 
 } /* namespace StockAction */
